f674bcc6d746fa621f28e44139590bb9baccf6f1
sjin4443 <71583444+sjin4443@users.noreply.github.com>
2020-10-24 23:01:20 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $(".big_airplane").click(function(){
+        $(".big_airplane_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".small_airplane").click(function(){
+        $(".small_airplane_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".boat").click(function(){
+        $(".boat_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+
+})
